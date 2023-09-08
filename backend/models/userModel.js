d7e350508494a4b103aff5284929aa17c25312cd
Siti Aisyah Binti Razali <aisyahrazali34@gmail.com>
2023-09-09 01:04:28 +0800

    initial commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+// define the data structure
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: Number,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    user_type: {
+        type: String,
+        required: true
+    },
+    muet_band: {
+        type: String,
+        required: false
+    },
+    start_session: {
+        type: String,
+        required: false
+    }
+}, { timestamps: true})
+
+module.exports = mongoose.model('User', userSchema)
