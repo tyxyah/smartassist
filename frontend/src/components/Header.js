91d327b71dcf9012ce66981f899745fa273e17c9
Siti Aisyah Binti Razali <aisyahrazali34@gmail.com>
2023-10-25 12:20:15 +0800

    update header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputAdornment from "@mui/material/InputAdornment";
 import Search from "@mui/icons-material/Search";
 import Clear from "@mui/icons-material/Clear";
-import Button from "@mui/material/Button"
+import Button from "@mui/material/Button";
 import { Grid } from "@mui/material";
 
 export default function UseFormControl() {
+  const [searchValue, setSearchValue] = useState(""); // State for the input value
+
+  const handleClearClick = () => {
+    setSearchValue(""); // Clear the search input when the "Clear" button is clicked
+  };
+
   return (
     <form noValidate autoComplete="on" position="fixed">
       <Box
@@ -17,9 +24,9 @@ export default function UseFormControl() {
         }}
       >
         <Grid container spacing={2}>
-          <Grid item xs={2.5}></Grid>
+          <Grid item xs={2.2}></Grid>
           <Grid item xs={7}>
-            <FormControl sx={{ margin: "13px", width: "30ch" }}>
+            <FormControl sx={{ margin: "13px", width: "35ch" }}>
               <OutlinedInput
                 size="small"
                 placeholder="Search"
@@ -30,14 +37,28 @@ export default function UseFormControl() {
                 }
                 endAdornment={
                   <InputAdornment position="end">
-                    <Button><Clear /></Button>
+                    <Button
+                      style={{
+                        cursor: "pointer",
+                        padding: 0, // Remove padding
+                        minWidth: "auto", // Set minimum width to "auto"
+                      }}
+                      classes={{
+                        root: "clear-button",
+                      }}
+                      onClick={handleClearClick} // Add onClick handler
+                    >
+                      <Clear />
+                    </Button>
                   </InputAdornment>
                 }
+                value={searchValue} // Controlled input value
+                onChange={(e) => setSearchValue(e.target.value)} // Update searchValue state
               />
             </FormControl>
           </Grid>
-          <Grid item xs={2.5}>
-            <p style={{ fontSize: "23px" }}>
+          <Grid item xs={2.7}>
+            <p style={{ fontSize: "23px", margin: "13px", textAlign: "right" }}>
               <strong>SMART</strong>
               <i>ASSIST</i>
             </p>
