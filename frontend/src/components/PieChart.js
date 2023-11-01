ffaf469e8a269d7d9d9a60b8a84b6b09984c45ef
Siti Aisyah Binti Razali <aisyahrazali34@gmail.com>
2023-11-01 11:21:55 +0800

    remove unused variable in piechart.js

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -25,26 +25,28 @@ function PieCenterLabel({ children }) {
 }
 
 export default function PieChartWithCenterLabel({ selectedCourseType }) {
-  const [courses, setCourses] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
-  const filteredCourses = courses.filter(
-    (course) => course.course_type === selectedCourseType
-  );
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Make a GET request to your API
         const response = await fetch("http://localhost:4000/api/course");
         if (response.ok) {
           const data = await response.json();
-          setCourses(data);
+          
+          // Filter courses based on selectedCourseType
+          const filteredCourses = data.filter(
+            (course) => course.course_type === selectedCourseType
+          );
+          
+          // Calculate total credit hours completed and remaining
           const totalCreditHrsCompleted = filteredCourses.reduce(
             (total, course) => total + parseInt(course.credit_hrs, 10),
             0
           );
-
           const remainingCreditHrs = 19 - totalCreditHrsCompleted;
-
+          
           setFilteredData([
             {
               value: totalCreditHrsCompleted,
@@ -57,38 +59,40 @@ export default function PieChartWithCenterLabel({ selectedCourseType }) {
           ]);
         } else {
           console.error("Failed to fetch data");
+          setFilteredData([]); // Clear the data on error
         }
       } catch (error) {
         console.error("Error:", error);
+        setFilteredData([]); // Clear the data on error
       }
     };
 
     fetchData();
-  });
-
-  if (filteredData.length === 0) {
-    return <div>Loading...</div>;
-  }
+  }, [selectedCourseType]);
 
   return (
     <div style={{ width: "100%" }}>
-      <PieChart
-      colors={palette}
-        series={[{ data: filteredData, innerRadius: 70 }]}
-        slotProps={{
-          legend: { hidden: true },
-        }}
-        {...size}
-      >
-        <PieCenterLabel>
-          <tspan x={size.width / 2} dy="-0.8em">
-            {filteredData[0].label}
-          </tspan>
-          <tspan x={size.width / 2} dy="1.6em">
-            {filteredData[1].label}
-          </tspan>
-        </PieCenterLabel>
-      </PieChart>
+      {filteredData.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <PieChart
+          colors={palette}
+          series={[{ data: filteredData, innerRadius: 70 }]}
+          slotProps={{
+            legend: { hidden: true },
+          }}
+          {...size}
+        >
+          <PieCenterLabel>
+            <tspan x={size.width / 2} dy="-0.8em">
+              {filteredData[0].label}
+            </tspan>
+            <tspan x={size.width / 2} dy="1.6em">
+              {filteredData[1].label}
+            </tspan>
+          </PieCenterLabel>
+        </PieChart>
+      )}
     </div>
   );
 }
