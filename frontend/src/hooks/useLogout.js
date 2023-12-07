f1f7629dcbe4e91f258e97d2a828162bcaeac465
Siti Aisyah Binti Razali <aisyahrazali34@gmail.com>
2023-12-07 14:00:32 +0800

    update useLogout.js

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from './useAuthContext'
+import { useCourseContext } from './useCourseContext'
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const { dispatch: courseDispatch } = useCourseContext()
     
     const logout = () => {
         //remove user from storage
@@ -9,6 +11,7 @@ export const useLogout = () => {
 
         //dispatch logout action
         dispatch({type: 'LOGOUT' })
+        courseDispatch({type: 'SET_COURSE', payload: null})
     }
 
     return {logout}
